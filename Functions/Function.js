b65471811ffce12b0ebb66304160c45bf295e899
Mahigavale <Maheshgavale07@gmail.com>
2024-12-10 14:41:45 +0530

    Function folder

---
+++ Functions/Function.js
@@ -0,0 +1,31 @@
+
+//function => reusable code block to compute something again and again.
+
+//code readability increased.
+
+//less code.
+
+//takes parameters so just change the params and values change.o/p change.
+
+//default values in parameters and overriding them.while actually invoking them.
+
+// Objects in js.the whole js is Object. Object is collecton of properties and values seperated by colon.
+
+//objects are created with the const keywords. so that we can't reassign them but 
+//change some of their properties.
+
+//Object has the property names and property values and can be used to hold the functions to perform specific operations on given properties of the object.
+
+//This :=> in object:=> Object scope , in function :=> Owner of function , Stand alone global scope
+
+//function object. ()=> execution
+
+//ways to create the Object  1)new Keyword 2)Object literal.
+
+
+
+
+
+
+
+
