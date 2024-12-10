1c3e952a30be8a65b53df008a39f0df4d8e4d320
Mahigavale <Maheshgavale07@gmail.com>
2024-12-10 22:35:14 +0530

    array methods'

---
+++ Array/ArrayMethods/Slice.js
@@ -0,0 +1,10 @@
+
+/*
+=> it is the copying method from the JS array.
+=> it is used to copy the elements from the original array to new array with,
+ start index, end index (end index is not included here !).
+
+ =>
+
+*/
+
---
+++ Array/ArrayMethods/Splice.js
@@ -0,0 +1,19 @@
+/*
+=> modifying method.
+
+=> modifies the original array.
+
+=>takes three arguments 1)start index 2)delete count 3)arguments to add.
+
+=>splice takes indices from the one and not from zero.
+
+=>when you take the zero index it adds the values in front of the first element.
+
+
+drawback : modifies the original array.
+
+*/
+
+
+
+
