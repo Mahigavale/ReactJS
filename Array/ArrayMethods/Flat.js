21800c20f369b9e0831d8c29400b70c50439a3b1
Mahigavale <Maheshgavale07@gmail.com>
2024-12-11 19:03:44 +0530

    array methods

---
+++ Array/ArrayMethods/Flat.js
@@ -0,0 +1,12 @@
+/*
+=> flat is used to flatten the given array or nested array to a specific depth.
+
+=>if we don't provide the depth it takes it as 1.
+
+=> Infinity  is also the depth of the given array.
+
+=>it  simplifies the nested array and makes it useful for the operations.
+*/
+
+
+
