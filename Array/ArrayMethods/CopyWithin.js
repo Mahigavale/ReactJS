0646dc03e19620db2ee356d20ce76f22725110ff
Mahigavale <Maheshgavale07@gmail.com>
2024-12-11 20:08:17 +0530

    array methods

---
+++ Array/ArrayMethods/CopyWithin.js
@@ -0,0 +1,11 @@
+/*
+=> as the name suggests this method is used to copy the elements of the array to the same array.
+
+=>it takes three arguments (target,start,lenghttocopy)
+
+=>overwrites only the given indices elements and keeps all the elements as it is.
+
+=>modifies the original array. :=> Modifying method.
+
+*/
+
