af7968858ddc09108e36a8fdaa276e124460d4cf
Mahigavale <Maheshgavale07@gmail.com>
2024-12-13 08:45:19 +0530

    Every

--- Array/ArrayMethods/Every.js
+++ Array/ArrayMethods/Every.js
@@ -9,3 +9,27 @@ Converse:=> Some.
 
 */
 
+
+let numbers=[11,13,15,17,19,21];
+
+
+
+ function Greater(n)
+ {
+   return n>=15;
+ }
+
+
+console.log(numbers.every(Greater));
+
+
+function Even(n)
+{
+    return n%2==0;
+}
+
+let arr2=[11,22,44,66,88,44];
+
+//
+console.log(arr2.every(Even));
+
