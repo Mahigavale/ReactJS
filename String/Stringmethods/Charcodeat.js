a96770e9d4d2bc8a981d808df397e74f7f1107a8
Mahigavale <Maheshgavale07@gmail.com>
2024-12-14 15:25:16 +0530

    String methods

---
+++ String/Stringmethods/Charcodeat.js
@@ -0,0 +1,7 @@
+/*
+=> returns the unicode value for the particular character at the given specified  index value.
+=> dosen't take the negative values as the argument.
+=>if you provide the negative value it will retunn Nan as o/p.
+*/
+
+
