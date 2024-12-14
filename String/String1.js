55e6c7488a1c714ef6ae05964abedcf84506086a
Mahigavale <Maheshgavale07@gmail.com>
2024-12-14 12:39:33 +0530

    the string Object

---
+++ String/String1.js
@@ -0,0 +1,39 @@
+
+/*
+=> basically Strings are the characters collection.
+=> Strings are important to work in real life.
+=> as Most of the times you found yourselves working with them.
+
+*/
+
+
+
+let str1='I am string !';
+
+let str2="I am String !";
+
+let str3=`I am   still a string `;
+
+
+let str4=new String("I am a string Object");
+
+let str5=String("I am a string but with the function invoked");
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(str5);
+
+
+console.log(typeof(str1));
+console.log(typeof(str2));
+console.log(typeof(str3));
+
+
+
+
+
+
+
+
