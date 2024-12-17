f4083f428e76af361556aeaf7b52c56186df7b89
Mahigavale <Maheshgavale07@gmail.com>
2024-12-17 15:58:04 +0530

    Closures in js

---
+++ Misc/Closures/First.js
@@ -0,0 +1,6 @@
+/*
+=>closure is nothing but the binding of the required data with the function itself.
+=> required data || lexical scope || surrounding state : ek hi bat hai !
+=> generally nested functions work with these closures.
+=> important concept.
+*/
