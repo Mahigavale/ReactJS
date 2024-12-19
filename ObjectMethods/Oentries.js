5b4284f7a871f07147f60d240740fd95e9a84918
Mahigavale <Maheshgavale07@gmail.com>
2024-12-19 22:19:09 +0530

    Object methods in js

---
+++ ObjectMethods/Oentries.js
@@ -0,0 +1,9 @@
+/**
+ * Object.keys()=>returns the array of the keys of the given object. in the form of the iterable.
+ * Object.values()=>returns the iterable of the values of the given Object.
+ * Object.entries()=>return the entries i.e. key and value pairs of the given Object.
+ * `${k}`,`${v}`
+ * Object.entries can be used to convert the given Object in the form of the Map.
+ * easy to iterate.
+ */
+
