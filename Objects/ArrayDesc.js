ba52e597cb979cd921598865ddbb69bb4646d931
Mahigavale <Maheshgavale07@gmail.com>
2024-12-22 16:12:42 +0530

    destrucuturing

---
+++ Objects/ArrayDesc.js
@@ -0,0 +1,48 @@
+
+
+let arr=['writing','reading','booking'];
+
+
+let[a,b,c,d="dadashree"]=arr;
+
+console.log(a);
+
+console.log(b);
+
+console.log(c);
+
+console.log(d);
+
+
+
+
+
+let person={
+    name:"mahesh",
+    email:"maheshgavale07@gmail.com",
+    mobile:"7774834341",
+    hobbies:["mahesh","Pooja","sachin","doom"]
+};
+
+
+
+let{name,mobile, hobbies:[h,i,,e]}=person ;
+
+console.log(name);
+
+console.log(mobile);
+
+console.log(h);
+console.log(i);
+
+console.log(e);
+
+
+
+
+
+
+
+
+
+
