b82236ddc4aa6fa4a11a9b57f16b8538b8b9ff57
Mahigavale <Maheshgavale07@gmail.com>
2024-12-22 17:35:26 +0530

    The combined destructuring

---
+++ Objects/Combinedesc.js
@@ -0,0 +1,23 @@
+
+
+let Profile={
+    name:"mahesh",
+    email:"maheshgavale07@gmail.com",
+    city:"Pune",
+    hobbies:["travelling","modelling","Writing","Cricket"]
+}
+
+
+let {name,city,hobbies:[a,b,c,d]}=Profile;
+
+
+console.log(name);
+
+console.log(city);
+
+// console.log(hobbies[0]);
+
+console.log(a,b,c,d);
+
+
+
