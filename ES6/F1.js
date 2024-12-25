90cf1c6db3adf68e4f7d8c4588213c03d5f9c50f
Mahigavale <Maheshgavale07@gmail.com>
2024-12-26 01:42:18 +0530

    ES6

---
+++ ES6/F1.js
@@ -0,0 +1,68 @@
+
+
+
+function demo(a,b)
+{
+    console.log(a+b);
+    
+};
+
+demo(100,200);
+
+
+
+function demo2(a,b)
+{
+    if(a%2==0 && b%2==0)
+    {
+        return a+b;
+    }
+    else{
+        return "condition failed !";
+    }
+    //unreachable code detected !
+};
+
+let result=demo2(13,17);
+
+let result_two=demo2(100,50);
+
+console.log(result);
+
+console.log(result_two);
+
+
+
+var res=function()
+{
+    console.log("hello world !");
+    
+};
+
+
+res();
+
+//values,arguments,Immediately invoked.=> anonymous functions.
+
+
+(function()
+{
+    console.log("IIFE function result here !");
+    
+})();
+//iife can be used to execute the functions which need to execute them firstly.
+
+// two types of anonymous functions.
+//1)IIFE and 2)ARROW FUNCTION
+
+
+let y=(X,Y)=>{console.log(X+Y)
+     console.log("Arrow function executed here !")};
+
+
+y(100,200);
+
+
+
+
+
