19cd8b663bafb9fbf654b02ba690e09ba6c84a4b
Mahigavale <Maheshgavale07@gmail.com>
2024-12-26 03:05:51 +0530

    ES6

--- ES6/F1.js
+++ ES6/F1.js
@@ -1,67 +1,88 @@
 
 
 
-function demo(a,b)
-{
-    console.log(a+b);
+// // function demo(a,b)
+// // {
+// //     console.log(a+b);
     
-};
+// // };
 
-demo(100,200);
-
-
-
-function demo2(a,b)
-{
-    if(a%2==0 && b%2==0)
-    {
-        return a+b;
-    }
-    else{
-        return "condition failed !";
-    }
-    //unreachable code detected !
-};
-
-let result=demo2(13,17);
-
-let result_two=demo2(100,50);
-
-console.log(result);
-
-console.log(result_two);
+// // demo(100,200);
 
 
 
-var res=function()
-{
-    console.log("hello world !");
+// // function demo2(a,b)
+// // {
+// //     if(a%2==0 && b%2==0)
+// //     {
+// //         return a+b;
+// //     }
+// //     else{
+// //         return "condition failed !";
+// //     }
+// //     //unreachable code detected !
+// // };
+
+// // let result=demo2(13,17);
+
+// // let result_two=demo2(100,50);
+
+// // console.log(result);
+
+// // console.log(result_two);
+
+
+
+// // var res=function()
+// // {
+// //     console.log("hello world !");
     
-};
+// // };
 
 
-res();
+// // res();
 
-//values,arguments,Immediately invoked.=> anonymous functions.
+// // //values,arguments,Immediately invoked.=> anonymous functions.
 
 
-(function()
-{
-    console.log("IIFE function result here !");
+// // (function()
+// // {
+// //     console.log("IIFE function result here !");
     
-})();
-//iife can be used to execute the functions which need to execute them firstly.
+// // })();
+// // //iife can be used to execute the functions which need to execute them firstly.
 
-// two types of anonymous functions.
-//1)IIFE and 2)ARROW FUNCTION
-
-
-let y=(X,Y)=>{console.log(X+Y)
-     console.log("Arrow function executed here !")};
+// // // two types of anonymous functions.
+// // //1)IIFE and 2)ARROW FUNCTION
 
 
-y(100,200);
+// // let y=(X,Y)=>{console.log(X+Y)
+// //      console.log("Arrow function executed here !")};
 
+
+// // y(100,200);
+
+
+
+
+
+
+// function *demo()
+// {
+//     yield 1;
+//     yield 2;
+//     return 4;
+//     yield 3;
+//     yield 4;
+// }
+
+// const gem=demo();
+
+// console.log(gem.next().done);
+// console.log(gem.next());
+// console.log(gem.next());
+// console.log(gem.next());
+// console.log(gem.next());
 
 
 
